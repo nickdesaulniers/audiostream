bf018d6a8b16f013b44da377f0bc643e2c6e94c7
Nick Desaulniers <wangstabill@snet.net>
2012-11-05 17:21:22 -0500

    don't append player until canplay event

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     var filename = encodeURIComponent(this.value).replace(/\.mp3$/, '.ogg');
     var audio = document.createElement('audio');
     audio.setAttribute('controls', null);
+    var listener = audio.addEventListener('canplay', function (e) {
+      document.body.appendChild(audio);
+      audio.removeEventListener('canplay', listener);
+    });
     audio.src = '/transcode/' + filename;
-    //audio.load();
-    document.body.appendChild(audio);
   });
 });
