484f9d63d0144fb8c121aa79db889385e3632f4a
Nick Desaulniers <wangstabill@snet.net>
2012-11-15 03:02:33 -0500

    added a progress meter but it doesn't work in mobile safari for shit

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -4,12 +4,16 @@ $(document).ready(function () {
     aaSorting: [[1, 'asc'], [3, 'asc']]
   });
   $(document).on('click', 'a.song_title', function (e) {
+    var div = document.getElementById('controls');
     e.preventDefault();
+    div.innerHTML = '';
+    div.appendChild(document.createElement('progress'));
     var filename = this.href.replace(/.+\/transcode\//, '');
     var audio = document.createElement('audio');
     audio.setAttribute('controls', null);
     var listener = audio.addEventListener('canplay', function (e) {
-      document.body.appendChild(audio);
+      div.innerHTML = '';
+      div.appendChild(audio);
       audio.removeEventListener('canplay', listener);
     });
     audio.src = '/transcode/' + filename;
