ce6980713a4a1f5f5b2d1db9c759f634a3dcd700
Nick Desaulniers <wangstabill@snet.net>
2012-11-15 18:31:24 -0500

    client side support

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -1,3 +1,36 @@
+// Detect what containers are supported
+// http://diveintohtml5.com/everything.html
+var audio_supported = mp3_supported = vorbis_supported = wav_supported =
+  aac_supported = false;
+
+function redirect (message) {
+  alert(message);
+  window.location = 'http://browsehappy.com/';
+}
+
+audio_supported = !!document.createElement('audio').canPlayType;
+if (audio_supported) {
+  var a = document.createElement('audio');
+  mp3_supported = !!a.canPlayType('audio/mpeg').replace(/no/, '');
+  ogg_supported =
+    !!a.canPlayType('audio/ogg; codecs="vorbis"').replace(/no/, '');
+  wav_supported = !!a.canPlayType('audio/wav; codecs="1"').replace(/no/, '');
+  aac_supported =
+    !!a.canPlayType('audio/mp4; codecs="mp4a.40.2"').replace(/no/, '');
+} else {
+  redirect('HTML5 audio not supported.');
+}
+
+// Preferred transcoded extension
+var supported_extension = '';
+(function () {
+  if (ogg_supported) return supported_extension = 'ogg';
+  if (mp3_supported) return supported_extension = 'mp3';
+  if (aac_supported) return supported_extension = 'aac'; // m4a, .mp4 ???
+  if (wav_supported) return supported_extension = 'ogg';
+  redirect('No support for ogg, mp3, aac, or wav');
+})();
+
 $(document).ready(function () {
   $('#table_id').dataTable({
     // This is super brittle
@@ -15,7 +48,6 @@ $(document).ready(function () {
       div.appendChild(audio);
       audio.removeEventListener('canplay', listener);
     });
-    var supported_extension = 'ogg';
     audio.src = '/transcode/' + supported_extension + '/' + this.dataset.filename;
   });
 });
