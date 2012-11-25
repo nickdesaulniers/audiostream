f5650dc67110ab7e72d26d93c1330e4751188149
Nick Desaulniers <wangstabill@snet.net>
2012-11-25 13:04:58 -0500

    seperated list code into another function

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -1,6 +1,18 @@
-function insertInNewTd (text) {
+function insertTextInNewTd (text) {
   var td = document.createElement('td');
   td.appendChild(document.createTextNode(text));
+  return td;
+}
+
+function insertSongInNewTd (title, id, ext) {
+  var td = document.createElement('td');
+  var a = document.createElement('a');
+  a.setAttribute('href', '#');
+  a.setAttribute('class', 'song_title');
+  a.setAttribute('data-songid', id);
+  a.setAttribute('data-ext', ext);
+  a.appendChild(document.createTextNode(title));
+  td.appendChild(a);
   return td;
 }
 
@@ -10,24 +22,15 @@ $(document).ready(function () {
   $.getJSON('/list', function (data) {
     for (var i = 0, len = data.length; i < len; i++) {
       var row = document.createElement('tr');
-      var td = document.createElement('td');
-      var a = document.createElement('a');
-
-      a.setAttribute('href', '#');
-      a.setAttribute('class', 'song_title')
-      a.setAttribute('data-songid', data[i].songID);
-      a.setAttribute('data-ext', data[i].ext);
-      a.appendChild(document.createTextNode(data[i].title));
-      td.appendChild(a);
-      row.appendChild(td);
-      
-      row.appendChild(insertInNewTd(data[i].artist));
-      row.appendChild(insertInNewTd(data[i].albumartist));
-      row.appendChild(insertInNewTd(data[i].album));
-      row.appendChild(insertInNewTd(data[i].year));
-      row.appendChild(insertInNewTd(data[i].track));
-      row.appendChild(insertInNewTd(data[i].genre));
-      row.appendChild(insertInNewTd(data[i].disk));
+      var song = data[i];
+      row.appendChild(insertSongInNewTd(song.title, song.songId, song.ext));
+      row.appendChild(insertTextInNewTd(song.artist));
+      row.appendChild(insertTextInNewTd(song.albumartist));
+      row.appendChild(insertTextInNewTd(song.album));
+      row.appendChild(insertTextInNewTd(song.year));
+      row.appendChild(insertTextInNewTd(song.track));
+      row.appendChild(insertTextInNewTd(song.genre));
+      row.appendChild(insertTextInNewTd(song.disk));
       target.appendChild(row);
     }
     $('#table_id').dataTable({
