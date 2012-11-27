eaf19f15e3451f0a982342dd39ffd94194fd7ea9
Nick Desaulniers <wangstabill@snet.net>
2012-11-27 01:23:19 -0500

    use localstorage for cache to speed up listing Issue #23

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -16,28 +16,66 @@ function insertSongInNewTd (title, id, ext) {
   return td;
 }
 
-$(document).ready(function () {
+// redoTable is new data for invalid cache
+function list (songs, redoTable) {
   var table = document.getElementById('table_id');
   var target = table.getElementsByTagName('tbody')[0];
   var fragment = document.createDocumentFragment();
-  $.getJSON('/list', function (data) {
-    for (var i = 0, len = data.length; i < len; i++) {
-      var row = document.createElement('tr');
-      var song = data[i];
-      row.appendChild(insertSongInNewTd(song.title, song.songID, song.ext));
-      row.appendChild(insertTextInNewTd(song.artist));
-      row.appendChild(insertTextInNewTd(song.albumartist));
-      row.appendChild(insertTextInNewTd(song.album));
-      row.appendChild(insertTextInNewTd(song.year));
-      row.appendChild(insertTextInNewTd(song.track));
-      row.appendChild(insertTextInNewTd(song.genre));
-      row.appendChild(insertTextInNewTd(song.disk));
-      fragment.appendChild(row);
+  for (var i = 0, len = songs.length; i < len; i++) {
+    var row = document.createElement('tr');
+    var song = songs[i];
+    row.appendChild(insertSongInNewTd(song.title, song.songID, song.ext));
+    row.appendChild(insertTextInNewTd(song.artist));
+    row.appendChild(insertTextInNewTd(song.albumartist));
+    row.appendChild(insertTextInNewTd(song.album));
+    row.appendChild(insertTextInNewTd(song.year));
+    row.appendChild(insertTextInNewTd(song.track));
+    row.appendChild(insertTextInNewTd(song.genre));
+    row.appendChild(insertTextInNewTd(song.disk));
+    fragment.appendChild(row);
+  }
+  
+  if (redoTable) {
+    redoTable.fnDestroy();
+    target.innerHTML = '';
+  }
+  
+  target.appendChild(fragment);
+  $('div#controls img:first').remove();
+  return $('#table_id').dataTable({
+    aaSorting: [[1, 'asc'], [3, 'asc']]
+  }).css('width', '').fadeIn(500);
+}
+
+// Cache miss
+function noCache (data) {
+  list(data);
+  localStorage.setItem('song_cache', JSON.stringify(data));
+}
+
+function validateCache (cached_songs, dataTable) {
+  $.getJSON('/list', function (server_songs) {
+    if (server_songs.length !== cached_songs.length ||
+    server_songs.toString().length !== cached_songs.toString().length ||
+    hex_md5(server_songs.toString()) !== hex_md5(cached_songs.toString())) {
+      // Invalid Cache
+      list(server_songs, dataTable);
+      localStorage.setItem('song_cache', JSON.stringify(server_songs));
     }
-    target.appendChild(fragment);
-    $('#table_id').dataTable({
-      aaSorting: [[1, 'asc'], [3, 'asc']]
-    }).fadeIn(500);
-    $('div#controls img:first').remove();
+    // Valid Cache
   });
+}
+
+$(document).ready(function () {
+  var songs, song_cache, song_cache_string = localStorage.getItem('song_cache');
+  if (!song_cache_string) return $.getJSON('/list', noCache);
+  try {
+    song_cache = JSON.parse(song_cache_string);
+  } catch (e) {
+    // Unparseable cache
+    localStorage.removeItem('song_cache');
+    return $.getJSON('/list', list);
+  }
+  // Cache hit
+  validateCache(song_cache, list(song_cache));
 });
