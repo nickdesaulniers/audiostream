205ac516b5c52a89a497a699b549c70cc906c364
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-12-29 22:38:23 -0500

    sort

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -10,13 +10,36 @@ function createSongListing (song_name, artist, duration) {
   return li;
 };
 
+function buildSort () {
+  var fields = arguments, len = fields.length;
+  return function (a, b) {
+    for (var i = 0, af, bf, field; i < len; ++i) {
+      field = fields[i];
+      af = a[field];
+      bf = b[field];
+      if (af < bf) return -1;
+      if (af > bf) return 1;
+      if (i === len - 1) return 0;
+    }
+  };
+};
+
 window.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("listing").parentElement.addEventListener("scroll", function (evt) {
+  var listing = document.getElementById("listing");
+  listing.parentElement.addEventListener("scroll", function (evt) {
     console.log(evt.target.scrollTop);
     if (evt.target.scrollTop === evt.target.scrollTopMax) {
       console.log("Reached Bottom");
     } else if (evt.target.scrollTop === 0) {
       console.log("Reached Top");
+    }
+  });
+
+  listing.addEventListener("click", function (e) {
+    if (e.target.nodeName === "SPAN") {
+      console.log(e.target.parentNode);
+    } else {
+      console.log(e.target);
     }
   });
 
@@ -30,12 +53,11 @@ window.addEventListener("DOMContentLoaded", function () {
     } catch (e) {
       return console.error(e);
     }
-    console.log(songs);
     var frag = document.createDocumentFragment();
-    songs.forEach(function (song) {
+    songs.sort(buildSort("artist", "album", "track")).forEach(function (song) {
       frag.appendChild(createSongListing(song.title, song.artist, song.duration));
     });
-    document.getElementById("listing").appendChild(frag);
+    listing.appendChild(frag);
   };
   xhr.send();
 });
