f8905ede68fd13f4708f986e22d0256d50dd290c
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-12-31 15:56:13 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,37 +1,27 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , config = require('./config/config');
-
+var express = require('express');
+var http = require('http');
+var routes = require('./routes');
+var config = require('./config/config');
 var app = express();
 
-app.configure(function(){
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+  app.disable('x-powered-by');
+  app.use(express.logger('dev'));
   app.use(express.compress());
   // HTTP Basic Authentication
   //app.use(express.basicAuth(config.username, config.password));
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
-  app.use(express.logger('dev'));
+  app.use(express.favicon(__dirname + 'public/img/favicon.ico'));
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', routes.index);
 app.get('/list', routes.list);
 app.get('/transcode/:extension/:fileID', routes.transcode);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
 });
+
