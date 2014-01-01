956a167cab6d112d78dcd40563cd1ac32a3c5542
Nick Desaulniers <ndesaulniers@mozilla.com>
2014-01-01 13:34:53 -0500

    update seek time

--- public/js/av.js
+++ public/js/av.js
@@ -2,8 +2,11 @@ var AV = {};
 
 AV.audio = new Audio;
 
-AV.play = function (ext, songID) {
+AV.play = function (ext, songID, cb) {
   this.audio.src = "/transcode/" + ext + "/" + songID;
   this.audio.play();
+  if (typeof cb === "function") {
+    this.audio.onloadedmetadata = cb.bind(null, this.audio);
+  }
 };
 
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,10 +1,14 @@
-var UI = {};
+var UI = {
+  seekMouseDown: false,
+};
 
-UI.play = function () { AV.audio.paused ? AV.audio.play() : AV.audio.pause(); };
+UI.play = function () {
+  AV.audio.paused ? AV.audio.play() : AV.audio.pause();
+};
 
 UI.playListing = function (e) {
   var target = e.target.nodeName === "SPAN" ? e.target.parentNode : e.target;
-  AV.play(target.dataset.ext, target.dataset.songID);
+  AV.play(target.dataset.ext, target.dataset.songID, UI.seekDuration);
 };
 
 UI.nowPlaying = function (e) {
@@ -12,14 +16,34 @@ UI.nowPlaying = function (e) {
   ele.style.zIndex = -1 * parseInt(getComputedStyle(ele).zIndex, 10);;
 };
 
-UI.volume = function (e) { AV.audio.volume = parseFloat(e.target.value); };
+UI.volume = function (e) { AV.audio.volume = e.target.valueAsNumber; };
+UI.seekDown = function () { UI.seekMouseDown = true; };
+UI.seekUp = function () { UI.seekMouseDown = false; };
+
+UI.timechange = function () {
+  if (!UI.seekMouseDown) {
+    document.getElementById("seek").value = AV.audio.currentTime;
+  }
+};
+
+UI.seekDuration = function (audio) {
+  var seek = document.getElementById("seek");
+  seek.min = seek.value = audio.seekable.start(0);;
+  seek.max = audio.seekable.end(0);
+};
 
 window.addEventListener("DOMContentLoaded", function () {
   var $ = document.getElementById.bind(document);
-  function clicked (id, cb) { $(id).addEventListener("click", cb); };
-  clicked("play", UI.play);
-  clicked("listing", UI.playListing);
-  clicked("playing", UI.nowPlaying);
-  clicked("volume", UI.volume);
+  function on (target, eventType, cb) {
+    if (typeof target === "string") target = $(target);
+    target.addEventListener(eventType, cb);
+  };
+  on("play", "click", UI.play);
+  on("listing", "click", UI.playListing);
+  on("playing", "click", UI.nowPlaying);
+  on("volume", "click", UI.volume);
+  on("seek", "mousedown", UI.seekDown);
+  on("seek", "mouseup", UI.seekUp);
+  on(AV.audio, "timeupdate", UI.timechange);
 });
 
